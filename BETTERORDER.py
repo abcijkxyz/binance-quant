daaba66d4e1eeba134f6c94c3cd4877736f7d0d7
duduzhu <duduzhu@gmail.com>
2017-12-07 00:55:26 +0800

    add an executor class to executor orders placing

--- BETTERORDER.py
+++ BETTERORDER.py
@@ -4,43 +4,25 @@ This strategy works by place an profitable order against a just filled one
 '''
 import apikey
 from dudubinance.accountcache import AccountCache
-from dudubinance.symbolsinfo import *
+from dudubinance.executor import Executor
 from binance.client import Client
-#from __future__ import print_function
 import json
 import threading
 import time
 
-stepratio = 0.002
-tradeamount = 100
-orderamount = 20
-baseasset = 'BNB'
-quoteasset = 'BTC'
-thesymbol = baseasset+quoteasset
+STEPRATIO = 0.002
+TRADEQUANTITY = 100
+ORDERAMOUNT = 20
+BASEASSET = 'BNB'
+QUOTEASSET = 'BTC'
+THESYMBOL = BASEASSET+QUOTEASSET
 
 client = Client(apikey.account['key'], apikey.account['secret'])
-refresh_symbolsinfo()
 
-ac = AccountCache(client, thesymbol)
+ac = AccountCache(client, THESYMBOL)
+executor = Executor(client)
 
 
-def safePlaceLimitOrder(NEWSIDE, SYMBOL, NEWQUANTITY,NEWPRICE):
-    try:
-        REALPRICE = format(round(NEWPRICE/symbolsInfo[SYMBOL]['tickSize']) * symbolsInfo[SYMBOL]['tickSize'],".8f")
-        print("{}:Placing an order: {} {} {}@{}".format(time.asctime( time.localtime(time.time()) ),NEWSIDE, SYMBOL, NEWQUANTITY,REALPRICE))
-        order = client.order_limit(
-            symbol=SYMBOL,
-            quantity=NEWQUANTITY,
-            side=NEWSIDE,
-            price=REALPRICE
-            #newOrderRespType='FULL'
-            )
-        time.sleep(0.11)
-    except ValueError:
-        pass
-    except NameError:
-        pass
-    #print(json.dumps(order, indent=4, sort_keys=True))
 
 def process_order_msg(msg):
     # if a limite order is filled
@@ -56,72 +38,46 @@ def process_order_msg(msg):
 
         if(msg['side'] == "BUY"):
             NEWSIDE = "SELL"
-            NEWPRICE = OLDPRICE/(1-stepratio)
+            NEWPRICE = OLDPRICE/(1-STEPRATIO)
         else:
             NEWSIDE = "BUY"
-            NEWPRICE = OLDPRICE*(1-stepratio)
+            NEWPRICE = OLDPRICE*(1-STEPRATIO)
 
-        safePlaceLimitOrder(NEWSIDE, msg['symbol'], msg['origQty'],NEWPRICE)
-
+        executor.safePlaceLimitOrder(NEWSIDE, msg['symbol'], msg['origQty'],NEWPRICE)
         orders = ac.getOrders(msg['symbol'])
         if len(orders[msg['side']]) == 0:
-            if msg['side'] == "BUY":
-                safePlaceLimitOrder("BUY", msg['symbol'], msg['origQty'],NEWPRICE*(1-stepratio))
+            depth = client.get_order_book(symbol=msg['symbol'])
             if msg['side'] == "SELL":
-                safePlaceLimitOrder("SELL", msg['symbol'], msg['origQty'],NEWPRICE/(1-stepratio))
+                basebalance = float(ac.getBalance(BASEASSET)['free'])
+                executor.placeOrderUntil("SELL",float(depth['asks'][0][0]),float(depth['asks'][0][0]) * 2,STEPRATIO,msg['symbol'],TRADEQUANTITY,basebalance,ORDERAMOUNT)
+            if msg['side'] == "BUY":
+                quotebalance = float(ac.getBalance(QUOTEASSET)['free'])
+                executor.placeOrderUntil("BUY",float(depth['bids'][0][0]),float(depth['bids'][0][0]) / 2,STEPRATIO,msg['symbol'],TRADEQUANTITY,quotebalance,ORDERAMOUNT)
 
 
-
-ac.registerOrderCallback(thesymbol,process_order_msg)
-
+ac.registerOrderCallback(THESYMBOL,process_order_msg)
 #fill 
-depth = client.get_order_book(symbol=thesymbol)
-orders = ac.getOrders(thesymbol)
+depth = client.get_order_book(symbol=THESYMBOL)
+orders = ac.getOrders(THESYMBOL)
 
 
 askprice = float(depth['asks'][0][0])
 myaskprice = askprice * 2
-basebalance = float(ac.getBalance(baseasset)['free'])
-print("my free {}: {}".format(baseasset,basebalance))
+
 if len(orders['SELL']) > 0:
     myaskprice = float(orders['SELL'][0]['price'])
     print("my lowest ask price: {}".format(myaskprice))
-i = 0
-while True:
-    if myaskprice/askprice <= (1+stepratio):
-        print myaskprice, askprice, stepratio
-        break;
-    if basebalance < tradeamount :
-        break;
-    safePlaceLimitOrder('SELL',thesymbol,tradeamount, askprice)
+basebalance = float(ac.getBalance(BASEASSET)['free'])
+print("my free {}: {}".format(BASEASSET,basebalance))
 
-    basebalance -= tradeamount
-    i+=1
-    askprice*=(1+stepratio)
-
-    if i > orderamount:
-        break;
+executor.placeOrderUntil("SELL",askprice,myaskprice,STEPRATIO,THESYMBOL,TRADEQUANTITY,basebalance,ORDERAMOUNT)
 
 bidprice = float(depth['bids'][0][0])
 mybidprice = bidprice/2
-quotebalance =  float(ac.getBalance(quoteasset)['free'])
-print("my free {}: {}".format(quoteasset,quotebalance))
 if len(orders['BUY']) > 0:
     mybidprice = float(orders['BUY'][0]['price'])
     print("my highest bidprice is {}".format(mybidprice))
+quotebalance =  float(ac.getBalance(QUOTEASSET)['free'])
+print("my free {}: {}".format(QUOTEASSET,quotebalance))
 
-i = 0
-while True:
-    if bidprice/mybidprice <= (1+stepratio):
-        print mybidprice, bidprice, stepratio
-        break;
-    if quotebalance < tradeamount*bidprice :
-        break;
-    safePlaceLimitOrder('BUY',thesymbol,tradeamount, bidprice)
-
-    quotebalance -= tradeamount*bidprice
-    i+=1
-    bidprice/=(1+stepratio)
-
-    if i > orderamount:
-        break;
+executor.placeOrderUntil("BUY",bidprice,mybidprice,STEPRATIO,THESYMBOL,TRADEQUANTITY,quotebalance,ORDERAMOUNT)
---
+++ dudubinance/executor.py
@@ -0,0 +1,51 @@
+from symbolsinfo import * 
+import time
+import sys
+class Executor(object):
+    def __init__(self, client):
+        refresh_symbolsinfo()
+        self._client = client
+
+    def safePlaceLimitOrder(self,NEWSIDE, SYMBOL, NEWQUANTITY,NEWPRICE):
+        REALPRICE = format(round(NEWPRICE/symbolsInfo[SYMBOL]['tickSize']) * symbolsInfo[SYMBOL]['tickSize'],".8f")
+        print("{}:Placing an order: {} {} {}@{}".format(time.asctime( time.localtime(time.time()) ),NEWSIDE, SYMBOL, NEWQUANTITY,REALPRICE))
+        try:
+            order = self._client.order_limit(
+            symbol=SYMBOL,
+            quantity=NEWQUANTITY,
+            side=NEWSIDE,
+            price=REALPRICE
+            #newOrderRespType='FULL'
+            )
+        except:
+            print "Unexpected error:", sys.exc_info()
+        time.sleep(0.11)
+
+    def placeOrderUntil(self,side,startprice,stopprice,thestepratio,targetsymbol,amount,assetbalance,maxorderamount):
+        nextprice = startprice
+        i = 0
+        while True:
+            if side=="SELL" and stopprice/nextprice <= (1+thestepratio):
+                print("Wanted to place sell at {}, but the stopprice is {},stop".format(nextprice, stopprice))
+                break;
+            if side=="BUY" and nextprice/stopprice <= (1+thestepratio):
+                print("Wanted to place buy at {}, but the stopprice is {},stop".format(nextprice, stopprice))
+                break;
+            if side=="SELL" and assetbalance < amount :
+                break;
+            if side=="BUY" and assetbalance < amount*nextprice:
+                break;
+
+            self.safePlaceLimitOrder(side,targetsymbol,amount, nextprice)
+
+            if side=="SELL":
+                assetbalance -= amount
+                nextprice*=(1+thestepratio)
+            if side=="BUY":
+                assetbalance -= amount*nextprice
+                nextprice/=(1+thestepratio)
+            i+=1
+
+            if i > maxorderamount:
+                break;
+
